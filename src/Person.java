e2c36aa015e4929c2ed54f6a623e8636998c5e1d
Rasmus <rasbalnor@gmail.com>
2017-02-28 09:03:25 +0100

    Project created

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Dunkl on 28/02/2017.
+ */
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
